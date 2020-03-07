6bb24876e1f0401c991ec3bd1a7a7dd173975658
Shiying Wang <fsywang@alumni.ubc.ca>
2020-03-06 18:24:56 -0800

    Code: more conditions, default X_test = None. Test: test invalid input, test output values

--- encoderpy/target_encoder.py
+++ encoderpy/target_encoder.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def target_encoder(X_train, X_test, y, cat_columns, prior = 0.5, objective = 'regression'):
         """
         This function encodes categorical variables with average target values for each category.
@@ -53,37 +56,65 @@ def target_encoder(X_train, X_test, y, cat_columns, prior = 0.5, objective = 're
         # check if y is a pandas series
         if isinstance(y, pd.Series) == False:
                 raise Exception("Type of y must be pd.Series")
-        #
-        if (isinstance(X_train, pd.DataFrame) & isinstance(X_test, pd.DataFrame)) == False:
-                raise Exception("Type of X_train and X_test must be pd.Dataframe")
-        # 
-        if (set(cat_columns).issubset(X_train.columns) & set(cat_columns).issubset(X_test.columns)) == False:
-                raise Exception("X_train and X_test must contain cat_columns")
-        
+        # check if length y equals to length X_train
+        if len(y) != len(X_train):
+                raise Exception("Input y must equal to X_train")
+        # check if X_train is pandas dataframe
+        if isinstance(X_train, pd.DataFrame) == False:
+                raise Exception("Type of X_train must be pd.Dataframe")
+        # check if X_train contains cat_columns
+        if set(cat_columns).issubset(X_train.columns) == False:
+                raise Exception("X_train must contain cat_columns")
+
+        y_new = y.copy()
         #check if target variable is numeric for regression objective
         if objective == 'regression':
                 if (y.dtype != 'int64') & (y.dtype != 'float64'):
-                        raise Exception("The target variable must be numeric.")
-        # for binary objective
+                raise Exception("The target variable must be numeric.")
+                # for binary objective
         else:
                 # check if target is binary
                 if y.nunique() != 2:
-                        raise Exception("The target variable must be binary")
+                raise Exception("The target variable must be binary")
                 # encode target to 0 or 1
                 if (y.dtype != 'int64') & (y.dtype != 'float64'):
-                        y = y.replace({y.unique()[0] : 0, y.unique()[1] : 1})
+                y_new = y.replace({y.unique()[0] : 0, y.unique()[1] : 1})
 
-        # make copy of original data
-        train_processed = X_train.copy()
-        test_processed = X_test.copy()
-
-        for col in cat_columns:
-                # calculate target mean for each category and save to dictionary
-                search_table = train_processed.groupby(col)[y.name].mean().to_dict()
+        if X_test is None:
+                train_processed = X_train.copy()
+                for col in cat_columns:
+                global_mean = np.nanmean(y_new)
+                # calculate target counts for each category and save to dictionary
+                search_table = train_processed.groupby(col)[y_new.name].aggregate(['sum', 'count'])
+                search_table['encodings'] = (search_table['sum'] + prior * global_mean) / (search_table['count'] + prior)
+                search_table = search_table.drop(columns=['sum', 'count'], axis=1).to_dict()['encodings']
                 # encode categorical columns for training dataset
                 train_processed.loc[:,col] = train_processed[col].map(search_table)
+                
+                return train_processed
+        
+        else:
+                # check if X_test is pandas dataframe
+                if isinstance(X_test, pd.DataFrame) == False:
+                raise Exception("Type of X_test must be pd.Dataframe")
+                # check if X_test contains cat_columns
+                if set(cat_columns).issubset(X_test.columns) == False:
+                raise Exception("X_test must contain cat_columns")
+                
+                train_processed = X_train.copy()
+                test_processed = X_test.copy()
+
+                for col in cat_columns:
+                global_mean = np.nanmean(y_new)
+                # calculate target counts for each category and save to dictionary
+                search_table = train_processed.groupby(col)[y_new.name].aggregate(['sum', 'count'])
+                search_table['encodings'] = (search_table['sum'] + prior * global_mean) / (search_table['count'] + prior)
+                search_table = search_table.drop(columns=['sum', 'count'], axis=1).to_dict()['encodings']
+                # encode categorical columns for training dataset
+                train_processed.loc[:,col] = train_processed[col].map(search_table)
+                
                 # encode categorical columns for testing dataset
                 test_processed.loc[:,col] = test_processed[col].map(search_table)
-                test_processed.loc[:,col] = test_processed[col].fillna(prior)  
-
-        return [train_processed, test_processed]
+                test_processed.loc[:,col] = test_processed[col].fillna(global_mean)
+                
+                return [train_processed, test_processed]
--- tests/test_target_encoder.py
+++ tests/test_target_encoder.py
@@ -1,8 +1,9 @@
 from encoderPy import target_encoder
 import pandas as pd
 import numpy as np
+import pytest
 
-data = pd.read_csv("https://raw.githubusercontent.com/UBC-MDS/encoderPy/fsywang_target_encoder/data/testing_data.csv")
+data = pd.read_csv("../data/testing_data.csv")
 
 train1 = data.query("train_test_1 == 'train'")
 test1 = data.query("train_test_1 == 'test'")
@@ -10,19 +11,68 @@ test1 = data.query("train_test_1 == 'test'")
 train2 = data.query("train_test_3 == 'train'")
 test2 = data.query("train_test_3 == 'test'")
 
+train_encode1, test_encode1 = target_encoder(X_train = train1, y = train1.target_bin, 
+                                             cat_columns= ['feature_cat_chr','feature_cat_num'],
+                                             X_test = test1, prior = 0.5, objective = 'binary')
+
+train_encode2, test_encode2 = target_encoder(X_train = train2, y = train2.target_bin, 
+                                             cat_columns= ['feature_cat_chr','feature_cat_num'],
+                                             X_test = test2, prior = 0.5, objective = 'binary')
+    
+target_cha = train1.target_bin.replace({train1.target_bin.unique()[0] : "a", train1.target_bin.unique()[1] : "b"})
+
+def check_exception():
+    #check if the function handles invalid inputs.
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = train1.target_bin, cat_columns= ['feature_cat_chr','feature_cat_num'],
+                       X_test = test1, prior = 0.5, objective = 'something')
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = train1.target_bin, cat_columns= "not list")
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = train1.target_bin, cat_columns= ['feature_cat_chr','feature_cat_num'],
+                       prior = 'string')
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = [1,2], cat_columns= ['feature_cat_chr','feature_cat_num'])
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = pd.Series([1,2]), cat_columns= ['feature_cat_chr','feature_cat_num'])
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = [1,2], y = train1.target_bin, cat_columns= ['something'])
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = target_cha, cat_columns= ['feature_cat_chr','feature_cat_num'])
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = train1.target_cont, cat_columns= ['feature_cat_chr','feature_cat_num'],
+                      objective = 'binary')
+        
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = train1.target_cont, cat_columns= ['feature_cat_chr','feature_cat_num'],
+                      X_test = [1,2])
+    
+    with pytest.raises(Exception):
+        target_encoder(X_train = train1, y = train1.target_cont, cat_columns= ['something'],
+                      X_test = test1)   
+        
+check_exception()
 
 def test_output():
-    train_encode1, test_encode1 = target_encoder(train1,test1,
-                                                 train.target_bin, 
-                                                 ['feature_cat_chr','feature_cat_num'])
-    train_encode2, test_encode2 = target_encoder(train2,test2,
-                                                 train.target_bin, 
-                                                 ['feature_cat_chr','feature_cat_num'], prior = 0.1)
+    # check if the outputs are correct.
+
     #test value
-    assert test_encode2['feature_cat_chr'].iloc[0] == 0.1, 'The encoded value for unseen test dataset should equal to prior'
-    assert np.isclose(train_encode1['feature_cat_chr'].iloc[0], 0.4285714) == True,'The encoded value for training dataset is wrong'
-    
+    assert train_encode1.feature_cat_chr.iloc[0] == 0.43, 'The encoded value for training dataset is wrong'
+    assert np.isclose(test_encode2['feature_cat_chr'].iloc[0], 0.5) == True,'The encoded value for unseen test dataset is wrong'
+
     #check shape
     assert train_encode1.shape == train1.shape, "The shape of training dataset is wrong"
     assert test_encode1.shape == test1.shape, "The shape of testing datset is wrong"
+    #check when X_test is none
+    assert target_encoder(X_train = train1, y = target_cha,cat_columns= ['feature_cat_chr','feature_cat_num'], 
+                objective = 'binary').shape == train1.shape
+
+test_output()
     
