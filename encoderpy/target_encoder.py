260f00098844b99d7041fd4ab0989a4f3db2a34b
Shiying Wang (Florence) <35549914+fsywang@users.noreply.github.com>
2020-03-09 15:45:37 -0700

    Update encoderpy/target_encoder.py
    Co-Authored-By: Brayden Tang <40974987+braydentang1@users.noreply.github.com>

--- encoderpy/target_encoder.py
+++ encoderpy/target_encoder.py
@@ -31,9 +31,9 @@ def target_encoder(X_train, y, cat_columns, X_test = None, prior = 0.5, objectiv
         train_processed : pd.DataFrame
                 The training set, with the categorical columns specified by the argument cat_columns
                 replaced by their encodings.
-        test_processed : pd.DataFrame (optional)
+        test_processed : pd.DataFrame 
                 The test set, with the categorical columns specified by the argument cat_columns
-                replaced by the learned encodings from the training set.
+                replaced by the learned encodings from the training set. This is an optional argument.
 
         Examples
         -------
