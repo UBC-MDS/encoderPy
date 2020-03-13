40e369c51d04eedfca58b4aaa408821c7ffc3a4f
Robert_Mac <Robert_Mac@Roberts-MBP.hsd1.tx.comcast.net>
2020-03-13 17:32:23 -0500

    Add bug fixes after review

--- encoderpy/onehot_encoder.py
+++ encoderpy/onehot_encoder.py
@@ -1,5 +1,4 @@
 def onehot_encoder(X_train, X_test, cat_columns):
-<<<<<<< HEAD
     """
     This function encodes categorical variables using the popular onehot method for each category.
     That is it will convert a category column in the dataframe to dummy binary values like this:
@@ -45,18 +44,21 @@ def onehot_encoder(X_train, X_test, cat_columns):
     """
   
     # Check that input is valid
-    if isinstance(X_train, pd.DataFrame) == False:
-        raise Exception("X_train must be a pandas Dataframe type")
-    if isinstance(X_test, pd.DataFrame) == False:
-        raise Exception("X_test must be a pandas Dataframe type")
+
     if isinstance(cat_columns, list) == False:
-            raise Exception("cat_columns must be a list type")
-
-
+        raise Exception("cat_columns must be a list type")
+    
     # Process X_train data
-    if X_train is not None:
-    # Add Assertion test
-
+    if X_train is None: 
+        
+        train_processed = X_train
+        
+    else:
+    
+    # Check that input is valid
+        if isinstance(X_train, pd.DataFrame) == False:
+            raise Exception("X_train must be a pandas Dataframe type")
+        
         data = X_train
         results = pd.DataFrame(np.nan, index = np.arange(data.shape[0]), columns = ['tobedeleted'])
 
@@ -78,8 +80,15 @@ def onehot_encoder(X_train, X_test, cat_columns):
         train_processed = results.drop(columns = ['tobedeleted']) # remove empty column created initially
 
     # Process X_test data
-    if X_test is not None:
-    # Add Assertion test
+    if X_test is None:
+        
+        test_processed = X_test
+    
+    else:
+        
+    # Check that input is valid
+        if isinstance(X_test, pd.DataFrame) == False:
+            raise Exception("X_test must be a pandas Dataframe type")
 
         data = X_test
         results = pd.DataFrame(np.nan, index = np.arange(data.shape[0]), columns = ['tobedeleted'])
@@ -112,42 +121,3 @@ def onehot_encoder(X_train, X_test, cat_columns):
     
 
   
-=======
-    """This function encodes categorical variables using the popular onehot
-    method for each category.
-
-
-    Parameters
-    ----------
-    X_train : pd.DataFrame
-            A pandas dataframe representing the training data set containing
-            some categorical features/columns.
-    X_test : pd.DataFrame
-            A pandas dataframe representing the test set, containing some set
-            of categorical features/columns.
-    cat_columns : list
-            The names of the categorical features to encode.
-
-    Returns
-    -------
-    train_processed : pd.DataFrame
-          The training set, with the categorical columns specified by the
-          argument cat_columns replaced by their encodings.
-    test_processed : pd.DataFrame
-          The test set, with the categorical columns specified by the
-          argument cat_columns replaced by the learned encodings from the
-          training set.
-
-    Examples
-    -------
-    >>> encodings = onehot_encoder(
-    my_train,
-    my_test,
-    cat_columns = ['foo'])
-
-    >>> train_new = encodings[0]
-
-    """
-
-    return [train_processed, test_processed]
->>>>>>> master
