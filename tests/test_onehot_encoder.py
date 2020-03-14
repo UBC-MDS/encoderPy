46d750a29b4bde9fd7d11878d9b2b6fc28fa9778
braydentang1 <brayden.tang1@gmail.com>
2020-03-14 00:01:36 -0700

    address flake8 build failure

--- tests/test_onehot_encoder.py
+++ tests/test_onehot_encoder.py
@@ -1,7 +1,6 @@
 # Load Libraries
 from encoderpy import onehot_encoder
 import pandas as pd
-import numpy as np
 import pytest
 
 
@@ -14,7 +13,7 @@ df['age2'] = df['age']*2
 df['test'] = df['treatment']
 
 # Define Train and Test Data Set
-X_train = df.drop(columns = ['effect'])
+X_train = df.drop(columns=['effect'])
 X_test = X_train
 
 # Define Category columns
@@ -22,34 +21,43 @@ categories1 = ['treatment', 'test']
 categories2 = ['treatment', 'age']  # One Categorical and One Interger Data
 
 # Encode data with categories 1
-train_encoded1, test_encoded1 = onehot_encoder.onehot_encoder(X_train, X_test, categories1)
-   
+train_encoded1, test_encoded1 = onehot_encoder.onehot_encoder(
+    X_train,
+    X_test, categories1)
+
 # Encode data with categories 2
-train_encoded1, test_encoded1 = onehot_encoder.onehot_encoder(X_train, X_test, categories2)
+train_encoded1, test_encoded1 = onehot_encoder.onehot_encoder(
+    X_train,
+    X_test,
+    categories2)
 
 
 def test_output():
-    """
-    """
-        
     # Perform unit tests:
-    
-    # 1) Check that output shape of is X_train processed is correct   
+
+    # 1) Check that output shape of is X_train processed is correct
     assert train_encoded1.shape == (36, 29),\
-    "The shape of processed training dataset is wrong: Check that your function is processing the training dataset correctly"
+        "The shape of processed training dataset is wrong: Check that your \
+        function is processing the training dataset correctly"
     assert test_encoded1.shape == (36, 29),\
-    "The shape of processed test dataset is wrong: Check that your function is processing the test dataset correctly"
-    
+        "The shape of processed test dataset is wrong: Check that your \
+        function is processing the test dataset correctly"
+
     # 2) Check there is not null values generated
-    assert train_encoded1.isnull().values.any() == False # Any Null values in the encoded train dataset
-    assert test_encoded1.isnull().values.any() == False # Any Null values in the encoded test dataset
-    
+
+    # Any Null values in the encoded train dataset
+    assert train_encoded1.isnull().values.any() is not True
+    # Any Null values in the encoded test dataset
+    assert test_encoded1.isnull().values.any() is not True
+
     # 3) Check that right columns are created
-    assert ("treatment_A" in train_encoded1) == True, "Check your function, it should generate a treatment_A column using the test data"
-    assert ("treatment_B" in train_encoded1) == True, "Check your function, it should generate a treatment_B column using the test data"
-    
-    
+    assert ("treatment_A" in train_encoded1) is True, "Check your function, \
+        it should generate a treatment_A column using the test data"
+    assert ("treatment_B" in train_encoded1) is True, "Check your function, \
+        it should generate a treatment_B column using the test data"
+
     return
+
 
 test_output()
 
@@ -61,26 +69,26 @@ def check_exception():
 
     # check if cat_columns is a list
     with pytest.raises(Exception):
-        target_encoder.onehot_encoder(
-            X_train=train1,
+        onehot_encoder.onehot_encoder(
+            X_train=X_train,
             X_test=None,
             cat_columns="not list")
-    
+
     # check if X_train is pandas dataframe
     with pytest.raises(Exception):
         onehot_encoder.onehot_encoder(
             X_train=[1, 2],
             X_test=None,
             cat_columns=['treatment', 'test'])
-        
+
     # check if X_test is pandas dataframe
     with pytest.raises(Exception):
         onehot_encoder.onehot_encoder(
-            X_train=None,
+            X_train=X_train,
             X_test=[1, 2],
             cat_columns=['treatment', 'age'])
-            
+
     return
 
-check_exception()
 
+check_exception()
